55ef2f1dac4c70f80101cd1519499f71d4fe8bf8
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2018-05-17 09:28:28 -0400

    refactored

--- emodel.py
+++ emodel.py
@@ -7,173 +7,280 @@ from pycrates import read_file
 
 
 
-infile = "ellipses.fits"
-#infile="k.fits"
-image_file = "img.fits"
-outfile = "model.fits"
-normalization = 1.0
-clobber = True
 
-
-def load_ellipses( infile ):
-
-    def scalar(x):
-        if len(x.shape) > 1:
-            x = x[:,0]
-        return x
+class ModelImage( object ):
     
-    tab = read_file(infile,"r")
-    
-    s = tab.get_column("shape").values
-    if not all([_s.lower() == 'ellipse' for _s in s]):
-        raise ValueError("Only ellipses, inclusive ellipses, are supported")
-    
-    x = scalar(tab.get_column("x").values)
-    y = scalar(tab.get_column("y").values)
-    r = tab.get_column("r").values
-    r0 = r[:,0]
-    r1 = r[:,1]
-    a = scalar(tab.get_column("rotang").values)
-    f = tab.get_column("fraction").values
-    
-    ee = [ ellipse(_x,_y,_r0,_r1,_a) for _x,_y,_r0,_r1,_a in zip(x,y,r0,r1,a)]
-    retval = [ (_f,_e) for _f,_e in zip(f,ee)]
-    retval.sort()
-    return retval
-
-
-def load_image(infile):
-    
-    img = read_file(infile,"r")
-    pixels = img.get_image().values
-    img.get_image().values = np.zeros_like(pixels)*1.0
-
-    _a = [x.lower() for x in img.get_axisnames()]
-
-    if 'sky' in _a:
-        sky = 'sky'
-    elif 'pos' in _a:
-        sky = 'pos'
-    else:
-        raise IOError("Unknown sky column in image")
-    
-    return(img,sky)
-
-
-def region_limits( e, xmax, ymax ):
-    b = e.extent()
-    retval = img.get_transform(sky).invert([[b["x0"],b["y0"]],[b["x1"],b["y1"]]])
-    retval = retval-1 # Convert to 0 based array indexing
-    retval = np.floor(retval)
-    retval = retval.astype("i4") + np.array( [[0,0],[1,1]]) # +1 to upper limit
-    retval = np.clip(retval, 1, (xmax,ymax))
-    return retval
-
-
-def convert_coordinates( img ):
-    ylen,xlen = img.get_image().values.shape
-    
-    ivals = [x+1.0 for x in range(xlen)]
-    jvals = [y+1.0 for y in range(ylen)]
-    ivals = np.array( ivals*ylen)
-    jvals = np.repeat( jvals,xlen)
-    ijvals = list(zip(ivals,jvals))
-    xyvals = img.get_transform(sky).apply( ijvals )
-    ijvals = [(int(i),int(j)) for i,j in ijvals ]
-    coords = dict(zip(ijvals, xyvals))
-    return coords
-
-
-def union_previous_shapes( edata, N ):
-
-    shapes_already_counted = CXCRegion()
-    for e in edata[0:N]:
-        shapes_already_counted = shapes_already_counted+e[1]
-    
-    return(shapes_already_counted)
-    
-
-
-    
-edata = load_ellipses( infile )
-img,sky = load_image(image_file)
-
-ylen,xlen = img.get_image().values.shape
-coords = convert_coordinates(img)
-
-ovals = img.get_image().values
-
-
-for N in range(0,len(edata)):
-
-    fN = edata[N][0] # fraction in 1st region
-    eN = edata[N][1] # the region itself
-
-
-    shapes_already_counted = union_previous_shapes(edata,N)
-
-    # Intersect current shape with existing ones.
-    region_already_counted = eN * shapes_already_counted
-
-    # Now we compute the elliptical "annulus" to fill in.
-    # We have to treat the 1st shape special.
-    if 0 == len(shapes_already_counted.shapes):
-        region_not_already_counted = eN
-    else:
-        region_not_already_counted = eN - shapes_already_counted
-
-
-    # Determine sum of fraction already included 
-    # in the intersection of the current ellipse and 
-    # the previous ellipses.  
-    erange = region_limits(region_already_counted,xlen, ylen)
-    current_frac = 0.0
-    for _y in range( erange[0][1], erange[1][1]+1):
-        for _x in range(erange[0][0], erange[1][0]+1):
-            pos = coords[(_x,_y)]
-            if region_already_counted.is_inside( pos[0],pos[1]):
-                current_frac = current_frac + ovals[_y-1,_x-1]
-
-    # The delta is the fraction 
-    delta_fN = fN - current_frac
-    if delta_fN < 0:
-        raise ValueError("Somethings wrong, no negative flux please")
-
-
-    
-    # Now we count how many pixels in the current image are 
-    # in the annulus.  We could use the .area() method but 
-    # this gets us what we need.
-    #
-    erange = region_limits(region_not_already_counted,xlen,ylen)
-    inside = []
-    weight = []
-    for _y in range( erange[0][1], erange[1][1]+1):
-        for _x in range(erange[0][0], erange[1][0]+1):
-            pos = coords[(_x,_y)]
-            if region_not_already_counted.is_inside( pos[0],pos[1]):
-                inside.append (  (_x-1,_y-1), )
-                weight.append( 1.0 )
-
-    #
-    # Due to pixel size, there may not be any pixels so we check
-    # otherwise, we evenly distibuted the delta-flux into that many
-    # pixels.
-    #
-    if len(inside)>0:
-        val = delta_fN / sum(weight)
-
-        # Save the values.
-        for _ij,_w in zip(inside,weight):
-            _i,_j = _ij
-            ovals[_j,_i] = ovals[_j,_i]+(val*_w)
+    def __init__( self, infile ):
+        self.infile = infile
+        self.ovals = None
+        self.xlen = None
+        self.ylen = None
+        self.sky = None
         
-    # DEBUG: img.write("{:03d}.fits".format(N),clobber=True)
+        self.load_image()
+        self.get_sky_axis()
+        self.convert_coordinates()
+
+        
+    def load_image(self):
+        """Load image
+        
+        We don't care about the image pixel values.
+        
+        TBD OK -- we do.  I will need them at some point to check for NaN/NULLs.
+        
+        The same input crate is used for the output.  
+        
+        TBD: Units/Content/Creator/etc.
+        
+        """
+
+        self._img_crate = read_file(self.infile,"r")
+        ### TBD: remove pixels, replace with self.ovals?
+        pixels = self._img_crate.get_image().values
+        self._img_crate.get_image().values = np.zeros_like(pixels)*1.0
+        self.ovals = self._img_crate.get_image().values
+        self.ylen,self.xlen = self.ovals.shape
 
 
-img.get_image().values = ovals * normalization
+    def get_sky_axis(self):
+        """Determine sky coord axis name"""
+        
+        _a = [x.lower() for x in self._img_crate.get_axisnames()]
+        if 'sky' in _a:
+            self.sky = 'sky'
+        elif 'pos' in _a:
+            self.sky = 'pos'
+        else:
+            raise IOError("Unknown sky column in image")
+        
 
-img.write(outfile, clobber=clobber)
+    def convert_coordinates(self):
+        """
+        Convert pixel coordinates to physical coordinates
+
+        Create (i,j) -> (x,y) mapping 
+        """
+        ivals = [x+1.0 for x in range(self.xlen)]
+        jvals = [y+1.0 for y in range(self.ylen)]
+        ivals = np.array( ivals*self.ylen) # this is why using lists instead of np.arrays
+        jvals = np.repeat( jvals,self.xlen)
+        ijvals = list(zip(ivals,jvals))
+        
+        # Due to memory leaks/etc, best to send all i,j in at once.
+        xyvals = self._img_crate.get_transform(self.sky).apply( ijvals )
+
+        # Cast to ints.  We don't want to before now, or crates/transform
+        # will use integers for x,y which we don't want.        
+        ijvals = [(int(i),int(j)) for i,j in ijvals ]
+        
+        self.coords = dict(zip(ijvals, xyvals))
+
+    def region_logical_limits( self, extent):
+        """
+        Convert extent from physical to image array indexes, clipping at
+        image boundaries.
+        """
+
+        limits = np.array( [[extent["x0"],extent["y0"]],
+                            [extent["x1"],extent["y1"]] ])                
+        retval = self._img_crate.get_transform(self.sky).invert(limits)
+        retval = retval-1 # Convert to 0 based array indexing
+        retval = np.floor(retval)
+        retval = retval.astype("i4") + np.array( [[0,0],[1,1]]) # +1 to upper limit
+        retval = np.clip(retval, 1, (self.xlen,self.ylen))
+
+        logical_limits = { 'xlo' : retval[0][0],
+                           'ylo' : retval[0][1],
+                           'xhi' : retval[1][0],
+                           'yhi' : retval[1][1] }
+
+        return logical_limits
+
+
+    def write( self, ovals, outfile, normalization, clobber):
+        """
+        
+        """
+        self._img_crate.get_image().values = ovals * normalization
+        self._img_crate.write(outfile, clobber=clobber)
+
+
+
+
+class EllipseModel(object):
+    
+    def __init__(self, infile):
+        self.infile = infile
+        self.ellipse_list = None
+        self.img = None
+        
+        self.load_ellipses()
+
+
+    def load_ellipses( self ):
+        """Load ellipses from region file.
+        
+        Convert to CXCRegion ellipses so we get all the wonderful
+        math logic
+        """
+
+        def _scalar(x):
+            if len(x.shape) > 1:
+                x = x[:,0]
+            return x
+        
+        tab = read_file(self.infile,"r")
+        s = tab.get_column("shape").values
+        x = _scalar(tab.get_column("x").values)
+        y = _scalar(tab.get_column("y").values)
+        r = tab.get_column("r").values
+        r0 = r[:,0]
+        r1 = r[:,1]
+        a = _scalar(tab.get_column("rotang").values)
+        f = tab.get_column("fraction").values
+        
+        if not all([_s.lower() == 'ellipse' for _s in s]):
+            raise ValueError("Only ellipses, inclusive ellipses, are supported")
+        ee = [ ellipse(_x,_y,_r0,_r1,_a) for _x,_y,_r0,_r1,_a in zip(x,y,r0,r1,a)]
+        retval = [ (_f,_e) for _f,_e in zip(f,ee)]
+        retval.sort() 
+        self.ellipse_list = retval
+
+
+    def load_image( self, infile ):
+        """
+        Load image and setup coordinates
+        """        
+        self.img = ModelImage(infile)
+        
+        
+    def union_previous_shapes( self, up_to_N ):
+        """
+        Combine ellipses with lower fractions.
+        """
+        shapes_already_counted = CXCRegion()
+        for e in self.ellipse_list[0:up_to_N]:
+            shapes_already_counted = shapes_already_counted+e[1]
+        
+        return(shapes_already_counted)
+        
+
+    def _calc_covered_frac(self, reg ):
+        # Determine sum of fraction already included 
+        # in the intersection of the current ellipse and 
+        # the previous ellipses.          
+        extent = reg.extent()
+        erange = self.img.region_logical_limits( extent )
+        current_frac = 0.0
+        for _y in range( erange["ylo"], erange["yhi"]+1):
+            for _x in range(erange["xlo"], erange["xhi"]+1):
+                pos = self.img.coords[(_x,_y)]
+                if reg.is_inside( pos[0],pos[1]):
+                    current_frac = current_frac + self.img.ovals[_y-1,_x-1]
+        return current_frac
+
+
+    def calc_area_and_weights( self, reg ):
+        # Now we count how many pixels in the current image are 
+        # in the annulus.  We could use the .area() method but 
+        # this gets us what we need.
+        #
+        extent = reg.extent()
+        erange = self.img.region_logical_limits( extent )
+        inside = []
+        weight = []
+        for _y in range( erange["ylo"], erange["yhi"]+1):
+            for _x in range(erange["xlo"], erange["xhi"]+1):
+                pos = self.img.coords[(_x,_y)]
+                if reg.is_inside( pos[0],pos[1]):
+                    inside.append (  (_x-1,_y-1), )
+                    weight.append( 1.0 )
+
+        return inside,weight
+
+
+    def _add_to_output(self, delta_fN, inside, weight ):
+        #
+        # Due to pixel size, there may not be any pixels so we check
+        # otherwise, we evenly distibuted the delta-flux into that many
+        # pixels.
+        #
+        if len(inside)>0:
+            val = delta_fN / sum(weight)
+
+            # Save the values.
+            for _ij,_w in zip(inside,weight):
+                _i,_j = _ij
+                self.img.ovals[_j,_i] = self.img.ovals[_j,_i]+(val*_w)
+
+
+    def process_region( self, N):
+        """
+        
+        """
+        fN = self.ellipse_list[N][0] # fraction in 1st region
+        eN = self.ellipse_list[N][1] # the region itself
+
+        shapes_already_counted = self.union_previous_shapes(N)
+
+        # Intersect current shape with existing ones.
+        region_already_counted = eN * shapes_already_counted
+        current_frac = self._calc_covered_frac( region_already_counted)
+
+        # The delta is the fraction 
+        delta_fN = fN - current_frac
+        if delta_fN < 0:
+            raise ValueError("Somethings wrong, no negative flux please")
+
+        # Now we compute the elliptical "annulus" to fill in.
+        # We have to treat the 1st shape special.
+        if 0 == len(shapes_already_counted.shapes):
+            region_not_already_counted = eN
+        else:
+            region_not_already_counted = eN - shapes_already_counted
+
+        inside, weight = self.calc_area_and_weights(region_not_already_counted)
+
+        self._add_to_output( delta_fN, inside, weight )
+
+
+    def doit(self):
+        """
+        """        
+        for N in range(len(self.ellipse_list)):
+            self.process_region(N)
+        
+
+    def write( self, outfile, normalization, clobber):
+        """
+        TODO clobber check
+        """
+        self.img.write( self.img.ovals, outfile, normalization, clobber)
+        
+
+
+def main():
+
+    infile = "ellipses.fits"
+    image_file = "img.fits"
+    outfile = "model.fits"
+    normalization = 1.0
+    clobber = True
+
+    my_ellipse = EllipseModel( infile )
+    my_ellipse.load_image( image_file )
+    my_ellipse.doit()
+    my_ellipse.write( outfile, normalization, clobber )
+
+
+main()
+
+
+
+    
+
+
 
 
     
