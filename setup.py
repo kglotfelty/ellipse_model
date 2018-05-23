bddf0b89be2844654e01cae906384848fb5b84ea
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2018-05-23 09:48:19 -0400

    add ahelp

--- setup.py
+++ setup.py
@@ -3,26 +3,25 @@
 import os
 import sys
 
-assert "ASCDS_INSTALL" in os.environ
+assert "ASCDS_INSTALL" in os.environ, "Please setup for CIAO before installing"
 
+# Set PYVER env variable so that it'll get replaced in setup.cfg
 ver = sys.version_info
-os.environ["PYVER"] = "python{}.{}".format(ver[0],ver[1])
+os.environ["PYVER"] = "python{}.{}".format(ver[0],ver[1]) 
+
 
 from distutils.core import setup
 
-
 setup( name='emodel',
-        version='0.1.0',
-        description='Elliptical model builder',
-        author='Anonymous',
-        author_email='WhoDat@cfa.harvard.edu',
-        url='https://github.com/kglotfelty/ellipse_model/',
-        py_modules=["masked_image_crate"],
-        scripts=["emodel"],
-        data_files=[('param',['emodel.par'])]
+       version='0.1.0',
+       description='Elliptical model builder',
+       author='Anonymous',
+       author_email='WhoDat@cfa.harvard.edu',
+       url='https://github.com/kglotfelty/ellipse_model/',
+       py_modules=["masked_image_crate"],
+       scripts=["emodel"],
+       data_files=[('param',['emodel.par']),
+                    ('share/doc/xml',['emodel.xml'])
+                    ]
                     
-        )
-"""
-plot_shapes.py
-setup.py
-"""
+    )
